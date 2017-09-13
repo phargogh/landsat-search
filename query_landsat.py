34b923e7bee6da41d2891ffc11c134328b6826cd
James Douglass <jdouglass@stanford.edu>
2017-09-13 00:13:05 -0700

    Loading rtree from a generator instead of by inserting.
    Yields a nice (but not order of magnitude) speedup.

--- query_landsat.py
+++ query_landsat.py
@@ -1,8 +1,11 @@
 import csv
 import json
+import time
 
 from rtree import index
 import requests
+
+# 29s
 
 
 SCENE_LIST = 'scene_list'
@@ -10,16 +13,20 @@ SCENE_LIST = 'scene_list'
 def main():
 
     print 'Loading spatial index'
-    scene_index = index.Index(interleaved=False)
-    with open(SCENE_LIST) as scenes:
-        reader = csv.DictReader(scenes)
-        for row_num, row in enumerate(reader):
-            bbox = tuple(float(row[attr]) for attr in ('min_lat', 'max_lat', 'min_lon', 'max_lon'))
-            data = dict((key, row[key]) for key in ('productId', 'cloudCover', 'path', 'row', 'download_url'))
-            scene_index.insert(row_num, bbox, obj=json.dumps(data))
-            if row_num > 500:
-                break
-    print 'Done.'
+    start_time = time.time()
+    def _load_data():
+        with open(SCENE_LIST) as scenes:
+            reader = csv.DictReader(scenes)
+            for row_num, row in enumerate(reader):
+                bbox = tuple(float(row[attr]) for attr in ('min_lat', 'max_lat', 'min_lon', 'max_lon'))
+                data = dict((key, row[key]) for key in ('productId', 'cloudCover', 'path', 'row', 'download_url'))
+                yield (row_num, bbox, json.dumps(data))
+
+    # Loading like so yields a 3x speedup.
+    scene_index = index.Index(_load_data(), interleaved=False)
+
+            #scene_index.insert(row_num, bbox, obj=json.dumps(data))
+    print 'Done. (%ss)' % round(time.time() - start_time, 2)
 
     intersecting_bbox = (68.40073, 75.74251, 24.4185, 35.93353)
     total_download_size = 0.
@@ -45,3 +52,5 @@ if __name__ == '__main__':
 #   * Filter by cloud cover
 #   * define lat/long bbox
 #   * define allowed processing collection levels (precision terrain [L1TP], systematic terrain [L1GS], systematic [L1GS]
+#   * limit the date range allowed
+#   * Limit to the first n results.
