254c48a11e1c7c8fe004b545673e9eee9f445719
James Douglass <jdouglass@stanford.edu>
2017-09-13 00:01:25 -0700

    Breaking after 500 entries, printing files and filesize.

--- query_landsat.py
+++ query_landsat.py
@@ -1,6 +1,10 @@
 import csv
 import json
+
 from rtree import index
+import requests
+
+
 SCENE_LIST = 'scene_list'
 
 def main():
@@ -13,6 +17,8 @@ def main():
             bbox = tuple(float(row[attr]) for attr in ('min_lat', 'max_lat', 'min_lon', 'max_lon'))
             data = dict((key, row[key]) for key in ('productId', 'cloudCover', 'path', 'row', 'download_url'))
             scene_index.insert(row_num, bbox, obj=json.dumps(data))
+            if row_num > 500:
+                break
     print 'Done.'
 
     intersecting_bbox = (68.40073, 75.74251, 24.4185, 35.93353)
@@ -22,12 +28,16 @@ def main():
             filename = "{product_id}_B{band}.TIF".format(
                 product_id=data['productId'],
                 band=band_num)
-            print data['download_url'].replace('index.html', filename)
+            raster_url = data['download_url'].replace('index.html', filename)
+            filesize = int(requests.head(raster_url).headers['Content-Length']) / 1024**2.
+            print "%55s (%s MB)" % (filename, round(filesize, 2))
+
 
 if __name__ == '__main__':
     main()
 
 # Need an argparse UI to:
+#   * Download latest scene list
 #   * Select bands
 #   * Filter by cloud cover
 #   * define lat/long bbox
